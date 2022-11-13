fe1f4fee98ba9f386f55718568bc6eb66a0f2797
a.nettleship@gmail.com <a.nettleship@gmail.com>
2022-11-13 20:19:01 +0000

    added route to api.py /add_user, todo, learn how to interact with db smoothly, errors currently result from handling db results object incorrectly

--- api/api.py
+++ api/api.py
@@ -1,6 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, request, current_app
 from app.app_factory import db
 from app.models import User
+from sqlite3 import IntegrityError
 
 api_blueprint = Blueprint("api_blueprint", __name__)
 
@@ -13,4 +14,24 @@ def index():
 @api_blueprint.route("/users")
 def user_list():
     users = db.session.execute(db.select(User).order_by(User.username)).scalars()
-    return users
+    return list(users)
+
+@api_blueprint.route("/add_user", methods=['GET'])
+def add_user():
+    username = request.args.get('username')
+    email = request.args.get('email')
+    last_user_num = db.session.execute(db.select(User).order_by(User.id)).first()
+    if last_user_num == None:
+        id = 0
+    else:
+        id = last_user_num[0].id + 1
+    this_user = User(id, email, username)
+
+    with current_app.app_context():
+        db.session.add(this_user)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            return f"{username} exists in database already"
+
+    return f"{username} with email address {email} added to index {id}"
