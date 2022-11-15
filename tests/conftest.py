4c700d057ef8e003a2b7f7729180b1d5590a7b01
a.nettleship@gmail.com <a.nettleship@gmail.com>
2022-11-15 19:31:26 +0000

    edit and fix existing tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,6 @@ from app.models import User
 
 
 @pytest.fixture(scope='module')
-def new_user():
-    user = User(1,'patkennedy79@gmail.com', 'FlaskIsAwesome')
+def test_user():
+    user = User(1,'emailuser@email.com', 'FlaskIsAwesome')
     return user
--- tests/test_unit_database.py
+++ tests/test_unit_database.py
@@ -1,14 +1,14 @@
 from app.models import User
-from tests.conftest import new_user
+from tests.conftest import test_user 
 
 
-def test_new_user(new_user):
+def test_new_user(test_user):
     """
     GIVEN a User model
     WHEN a new User is created
     THEN check the email, hashed_password, and role fields are defined correctly
     """
-    user = User(1,'patkennedy79@gmail.com', 'FlaskIsAwesome')
-    assert user.email == 'patkennedy79@gmail.com'
+    user = User(1,'emailuser@email.com', 'FlaskIsAwesome')
+    assert user.email == 'emailuser@email.com'
     assert user.username == 'FlaskIsAwesome'
     assert user.id == 1
