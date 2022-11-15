eec59cd59ee66cfe6361dd2d8b8bc5f20eecf0f5
a.nettleship@gmail.com <a.nettleship@gmail.com>
2022-11-15 20:28:35 +0000

    change pytest fixture scope to function, to instantiate db models anew for each test

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,19 +5,19 @@ from app.app_factory import db
 from app.models import User
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope='function')
 def test_user():
     user = User(0,'emailuser@email.com', 'FlaskIsAwesome')
     return user
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope='function')
 def test_user1():
     user = User(1,'emailuser1@email.com', 'FlaskIsAwesome1')
     return user
 
 
-@pytest.fixture(scope='module')
+@pytest.fixture(scope='function')
 def test_user2():
     user = User(2,'emailuser2@email.com', 'FlaskIsAwesome2')
     return user
